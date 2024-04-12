01a81dea319979b0ac27cd8958721577cd3b5a7b
Lucas-Merino <merino.lucas20@gmail.com>
2024-04-12 05:13:58 -0300

    End of section 6

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,24 +1,62 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from '@mui/material'
 
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { Product } from '../../app/models/product';
 import agent from '../../app/api/agent';
 import NotFound from '../../app/errors/NotFound';
 import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useStoreContext } from '../../app/context/StoreContext';
+import { LoadingButton } from '@mui/lab';
 
 const ProducDetails = () => {
     const {id} = useParams<{id: string}>();
+    const {basket, setBasket, removeItem} = useStoreContext();
 
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
     
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmittting] = useState(false);
+
+    const item = basket?.items.find(i => i.productId === product?.id);
+
     useEffect(() => {
+        if(item){
+            setQuantity(item.quantity);
+        }
         id && agent.Catalog.details(Number(id))
         .then(res => setProduct(res))
         .catch(error => console.log(error))
         .finally(() => setLoading(false))
-    }, [id])
+    }, [id, item])
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        if(parseInt(event.currentTarget.value) >= 0){
+            setQuantity(parseInt(event.currentTarget.value));
+        }
+    }
+
+    const handleUpdateCart = () => {
+        if(!product) return;
+        
+        setSubmittting(true)
+        
+        if(!item || quantity > item.quantity){
+            const updatedQuantity = item ? quantity - item.quantity : quantity;
+            
+            agent.Basket.addItem(product.id, updatedQuantity)
+                .then(basket => setBasket(basket))
+                .catch(error => console.log(error))
+                .finally(() => setSubmittting(false))
+        } else {
+            const updatedQuantity = item.quantity - quantity;
+            agent.Basket.removeItem(product.id, updatedQuantity)
+                .then(() => removeItem(product.id, updatedQuantity))
+                .catch(error => console.log(error))
+                .finally(() => setSubmittting(false))
+        }
+    }
 
     if (loading) return <LoadingComponent message='Loading product...' />
  
@@ -58,6 +96,32 @@ const ProducDetails = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <TextField 
+                            onChange={(e) => handleInputChange(e)}
+                            variant='outlined'
+                            type='number'
+                            label='Quantity in Cart'
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton
+                            disabled={item?.quantity === quantity || (!item && quantity === 0)}
+                            loading={submitting}
+                            onClick={handleUpdateCart}
+                            sx={{height: '55px'}}
+                            color='primary'
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                        >
+                            {item ? 'Update Quantity' : 'Add to Cart'}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
